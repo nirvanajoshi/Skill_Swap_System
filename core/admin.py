07d6f08d2cd2a4ad93e4cd77bc79953a2035f912
nirvanajoshi <damodarj36@gmail.com>
2026-08-02 20:41:16 +0545

    Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, SkillOffered, SkillWanted, SwapRequest, Feedback
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(SkillOffered)
+admin.site.register(SkillWanted)
+admin.site.register(SwapRequest)
+admin.site.register(Feedback)
